c36f9c17206bf55961504575e00c016c6eef4336
Tao Man Wai <o_otaotao@hotmail.com>
2015-11-26 18:25:39 +0800

    bak

--- app/src/androidTest/java/com/tommytao/a5steak/sample/PlacesApiManagerTest.java
+++ app/src/androidTest/java/com/tommytao/a5steak/sample/PlacesApiManagerTest.java
@@ -234,6 +234,8 @@ public class PlacesApiManagerTest extends ApplicationTestCase<Application> {
 
 
 
+
+
     public void testSearchPlacesInDetail_shouldReturnEnWhenLocaleEn() throws Exception {
 
         final CountDownLatch signal = new CountDownLatch(1);
