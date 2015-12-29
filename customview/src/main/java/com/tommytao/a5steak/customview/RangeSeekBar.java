6c8aa5b8553f74c769b34444a0c6cc2949158aba
Tao Man Wai <o_otaotao@hotmail.com>
2015-12-29 11:34:26 +0800

    change RangeSeekBar to green color for Techson hotel project

--- customview/src/main/java/com/tommytao/a5steak/customview/RangeSeekBar.java
+++ customview/src/main/java/com/tommytao/a5steak/customview/RangeSeekBar.java
@@ -49,7 +49,7 @@ public class RangeSeekBar<T extends Number> extends ImageView {
     /**
      * Default color of a {@link RangeSeekBar}, #FF33B5E5. This is also known as "Ice Cream Sandwich" blue.
      */
-    public static final int DEFAULT_COLOR = Color.argb(0xFF, 0x33, 0xB5, 0xE5);
+    public static final int DEFAULT_COLOR = Color.argb(0xFF, 0x77, 0xB2, 0x55); // 0x33, 0xB5, 0xE5
 
     /**
      * An invalid pointer id.
